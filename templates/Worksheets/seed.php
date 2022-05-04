4fcd4ba87983e01bec2622b3aceba7f24aae9758
Emilien Kopp <emilien.kopp@outlook.com>
2022-05-05 00:58:02 +0900

    added seeding of worksheets and automatic creation of subranks for each new rank added

---
+++ templates/Worksheets/seed.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ */
+?>
+<div class="row">
+    <aside class="column">
+        <div class="side-nav">
+            <h4 class="heading"><?= __('Actions') ?></h4>
+            <?= $this->Html->link(__('List Worksheets'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+        </div>
+    </aside>
+    <div class="column-responsive column-80">
+        <div class="worksheets form content">
+            <?= $this->Form->create($worksheet, ['url' => ['action' => 'seed']]) ?>
+            <fieldset>
+                <legend><?= __('Add Worksheet') ?></legend>
+                <?php
+                    echo $this->Form->control('rank_id', ['options' => $ranks]);
+                ?>
+            </fieldset>
+            <?= $this->Form->button(__('Submit')) ?>
+            <?= $this->Form->end() ?>
+        </div>
+    </div>
+</div>
